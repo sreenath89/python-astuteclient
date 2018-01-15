855a74805eb01a5c46e87f114e008f7b90a0c8e4
Sreenath <sreenath.mm@poornam.com>
2018-01-15 17:56:58 +0530

    Update and Delete functions

--- astuteclient/v1/shell.py
+++ astuteclient/v1/shell.py
@@ -135,8 +135,7 @@ def do_billing_type_update(cc, args):
 
 @utils.arg(
     '--billing_type_id', 
-    metavar='<ID of Billing Type>', 
-    type=int,
+    metavar='<ID of Billing Type>',
     help='ID of the billing type which is to be deleted.')
 
 def do_billing_type_delete(cc, args):
